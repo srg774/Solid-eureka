73a22aa48315e3757681c1f6f7e4ccad46a8677e
srg774 <75862558+srg774@users.noreply.github.com>
2024-09-14 17:50:08 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const canvas = document.getElementById('gameCanvas');
     const ctx = canvas.getContext('2d');
 
-    canvas.width = 480;
+    canvas.width = 440;
     canvas.height = 800;
 
     let isGameOver = false;
